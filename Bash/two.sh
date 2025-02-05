816acd553dd1511e8ec069918b658d7a248794ef
fatimah63 <fatimahyusuff18@gmail.com>
2025-02-05 18:06:04 +0000

    adding to my bash script

---
+++ Bash/two.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e 
+
+mkdir -p /Users/admin/temp
+
+cd /Users/admin/temp 
+touch newfile1.txt
+
+echo "Hello, welcome to my github, hope you like it :)" > newfile1.txt
+
+cat newfile1.txt
+
+
