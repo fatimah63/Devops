6aaa519101e975b8de0f6e317b0b1ccba9e418c8
fatimah63 <fatimahyusuff18@gmail.com>
2025-02-05 17:03:18 +0000

    adding scripts

---
+++ Bash/one.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+num1=4
+num2=8
+
+result=$(($num2*num1))
+
+echo "The sum of $num2 multiplied by $num1 is:$result"
+
+num3=9
+num4=8
+
+result=$(($num3-$num4))
+
+echo "The sum of $num3 minus $num4 is:$result"
+
+num5=13
+num6=3
+
+result=$(($num5-$num6))
+
+echo "The sum of $num5 minus $num6 is:$result"
+
+
