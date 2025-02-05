adeaf50b18bfade76232f077e2294ea2bec726a3
fatimah63 <fatimahyusuff18@gmail.com>
2025-02-05 18:22:35 +0000

    adding to my bash scripts

---
+++ Bash/three.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+touch file.txt
+
+
+newfile=file.txt
+
+if [ -e $newfile ];then
+echo "This file exists."
+cat $newfile
+else
+  echo "This file does not exist. Try again later"
+fi
+
+
